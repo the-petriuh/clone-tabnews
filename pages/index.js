747b567366ddce53892fc9db6d86d1654e38c163
Daniel Petriu <danielpetriu75@gmail.com>
2024-11-26 12:29:13 -0300

    atualiza a mensagem

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Bem Vindo! aqui começa a sua jornada para o sucesso profissional</h1>
+    return <h1>Bem Vindo! aqui começa a sua jornada para o sucesso profissional 😎😁</h1>
 }
 
 export default Home;
