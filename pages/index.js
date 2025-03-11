943596a5741a0e4cad3ea3c625c7aa45caab9e60
Daniel Petriu <danielpetriu75@gmail.com>
2025-03-10 21:10:28 -0300

    removes friends names from index.js

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,6 @@ function Home() {
       <h1>
         Bem Vindo! aqui começa a sua jornada para o sucesso profissional 😎😁
       </h1>
-      <p> Dedico este projeto aos meus amigos Marlom e Derick</p>
-      <p>Derick Fraco e Marlom Indisciplinado</p>
     </div>
   );
 }
