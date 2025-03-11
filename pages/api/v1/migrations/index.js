e638c6d92eaff772b088670f1063c9be0f85b064
Daniel Petriu <danielpetriu75@gmail.com>
2025-03-11 20:46:43 -0300

    add tests for other http methods

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,36 +1,42 @@
 import migrationRunner from "node-pg-migrate";
 import path from "node:path";
 import database from "infra/database.js";
+import { error } from "node:console";
 
 export default async function migrations(request, response) {
   if (!["GET", "POST"].includes(request.method)) {
-    return response.status(405).end();
+    return response.status(405).json({
+      error: `Method ${request.method} not allowed`,
+      allowedMethods: ["GET", "POST"],
+    });
   }
 
   const isDryRun = request.method === "GET";
+  let dbClient;
+  try {
+    dbClient = await database.getNewClient();
 
-  console.log("database url", process.env.DATABASE_URL);
+    const defaultMigrationOptions = {
+      dbClient: dbClient,
+      dir: path.join("infra", "migrations"),
+      dryRun: isDryRun,
+      direction: "up",
+      verbose: true,
+      migrationsTable: "pgmigrations",
+    };
 
-  const dbClient = await database.getNewClient();
+    const migrations = await migrationRunner({
+      ...defaultMigrationOptions,
+    });
 
-  const defaultMigrationOptions = {
-    dbClient: dbClient,
-    dir: path.join("infra", "migrations"),
-    dryRun: isDryRun,
-    direction: "up",
-    verbose: true,
-    migrationsTable: "pgmigrations",
-  };
+    if (migrations.length > 0) {
+      return response.status(201).json(migrations);
+    }
 
-  const migrations = await migrationRunner({
-    ...defaultMigrationOptions,
-  });
-
-  await dbClient.end();
-
-  if (migrations.length > 0) {
-    return response.status(201).json(migrations);
+    return response.status(200).json(migrations);
+  } catch (exception) {
+    throw exception;
+  } finally {
+    await dbClient.end();
   }
-
-  return response.status(200).json(migrations);
 }
---
+++ tests/integration/api/v1/migrations/patch.test.js
@@ -0,0 +1,15 @@
+import database from "infra/database.js";
+
+beforeAll(cleanDatabase);
+
+async function cleanDatabase() {
+  await database.query("drop schema public cascade; create schema public;");
+}
+
+test("PATCH to api/v1/migrations should return 200", async () => {
+  // first request
+  const response1 = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "PATCH",
+  });
+  expect(response1.status).toBe(405);
+});
