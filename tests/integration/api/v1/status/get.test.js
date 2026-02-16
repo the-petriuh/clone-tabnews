4c5a1bac554486866bbc5f3cfad6144416e8fc2a
Daniel Petriu <danielpetriu75@gmail.com>
2026-02-16 18:09:07 -0300

    test: removes error for testing CI

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -18,7 +18,7 @@ test("GET to api/v1/status should return 200", async () => {
   const parsedUpdateAt = new Date(responseBody.updated_at).toISOString();
   expect(responseBody.updated_at).toBe(parsedUpdateAt);
 
-  expect(responseBody.dependencies.database.version).toEqual("17.3");
+  expect(responseBody.dependencies.database.version).toEqual("17.2");
   expect(responseBody.dependencies.database.max_connections).toEqual(100);
   expect(responseBody.dependencies.database.opened_connections).toEqual(1);
 });
